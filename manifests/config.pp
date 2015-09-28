1279a3124e80bd7533ac002c9a9dfbba01c5de64
Matt Simmons <matthew.simmons@spacex.com>
2015-09-28 15:03:53 -0700

    Added in some testing to guarantee that the assumed string is actually a string

--- manifests/config.pp
+++ manifests/config.pp
@@ -58,15 +58,19 @@ class racktables::config (
     }
 
     default: { # Assume the $secretfile is the string content
-      file { "${datadir}/wwwroot/inc/secret.php":
-        ensure  => present,
-        owner   => $apacheuser,
-        mode    => '0400',
-        seluser => 'system_u',
-        selrole => 'object_r',
-        seltype => 'httpd_sys_content_t',
-        require => Vcsrepo[$datadir],
-        content => $secretfile,
+      if is_string($secretfile) { 
+        file { "${datadir}/wwwroot/inc/secret.php":
+          ensure  => present,
+          owner   => $apacheuser,
+          mode    => '0400',
+          seluser => 'system_u',
+          selrole => 'object_r',
+          seltype => 'httpd_sys_content_t',
+          require => Vcsrepo[$datadir],
+          content => $secretfile,
+        }
+      } else { 
+        fail("secretfile is not a string")
       }
     }
 
